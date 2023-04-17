d935d8c96b6aa190a54301544fe038645ea6b134
Vincenzo-Vicchiariello <v.vicchiariello@outlook.it>
2023-04-17 17:05:30 +0200

    Aggiungere i file di progetto.

---
+++ Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class myRectangle
+    {
+        public int rectangleBase;
+        public int rectangleHeight;
+        public int rectangleArea;
+        public int rectanglePerimeter;
+        public myRectangle(int rectangleBase, int rectangleHeight)
+        {
+            this.rectangleBase = rectangleBase;
+            this.rectangleHeight = rectangleHeight;
+        }
+
+
+    
+        public int AreaCalc (int x, int y)
+        {
+            rectangleArea = x * y;
+            return rectangleArea;
+
+        }
+
+        public int PerimeterCalc (int x, int y) {
+            rectanglePerimeter = x * 2 + y * 2;
+            return rectanglePerimeter;
+        }
+
+
+    }
+}
+
+
+
+ 
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+
+Console.WriteLine("Inserisci la base del rettangolo in cm.");
+int myRectangleBase = int.Parse(Console.ReadLine());
+Console.WriteLine($"La tua base è di {myRectangleBase} cm");
+
+Console.WriteLine("Inserisci l'altezza del rettangolo in cm.");
+int myRectangleHeight = int.Parse(Console.ReadLine());
+Console.WriteLine($"La tua altezza è di {myRectangleHeight} cm");
+
+myRectangle rectangle1 = new myRectangle(myRectangleBase, myRectangleHeight);
+
+
+
+
+int finalResultArea = rectangle1.AreaCalc(myRectangleBase, myRectangleHeight);
+int finalResultPerimeter = rectangle1.PerimeterCalc(myRectangleBase, myRectangleHeight);  
+
+Console.WriteLine($"L'area del tuo rettangolo è di {finalResultArea} cm2");
+Console.WriteLine($"Il perimetro del tuo rettangolo è di {finalResultPerimeter} cm.");
